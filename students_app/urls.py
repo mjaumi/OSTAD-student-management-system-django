b17c78251c0fde279a5e304c11a3f90cc1a76906
mjaumi <mjaumi2864@gmail.com>
2025-03-13 23:09:49 +0600

    updating existing student feature added

--- students_app/urls.py
+++ students_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add, name='add_student'),
+    path('update/<int:student_id>/', views.update, name='update_student'),
 ]
--- students_app/views.py
+++ students_app/views.py
@@ -21,3 +21,20 @@ def add(request):
         
 
     return render(request, 'add_student.html')
+
+
+def update(request, student_id):
+    student = Student.objects.get(id=student_id)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone_no = request.POST.get('phone_no')
+        course_name = request.POST.get('course_name')
+
+        Student.objects.filter(id=student_id).update(name=name, email=email, phone_no=phone_no, course_name=course_name)
+        messages.success(request, 'Student Updated Successfully!!')
+        
+        return redirect('/')
+
+    return render(request, 'update_student.html', {'student': student})
